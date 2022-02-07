072ce1629d14085fc804069c17680f4296784dd7
pdrsvr <pedro.hbessa@live.com>
2022-02-07 18:29:02 -0300

    adding new documents to server

--- src/components/Creator.jsx
+++ src/components/Creator.jsx
@@ -1,31 +1,44 @@
-import React, { useState } from "react";
+import db from "../firebase";
+import { collection, addDoc, onSnapshot } from "firebase/firestore";
+import { Navigate } from "react-router-dom";
 
 import "../styles/creator.scss";
+import { useState } from "react";
 
 const Creator = (props) => {
   const handleChange = (event) => {};
 
-  console.log("aqui");
+  const [onSubmit, setOnsubmit] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log(event);
+    addDoc(collection(db, "events"), {
+      title: event.target.elements.title.value,
+      description: event.target.elements.description.value,
+    });
+
+    setOnsubmit(event.target.elements);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <div>
-          <p>TÍTULO:</p>
-        </div>
-        <input type="text" onChange={handleChange} />
-      </label>
-      <label>
-        <div>
-          <p>DESCRIÇÃO:</p>
-        </div>
-        <input type="text" onChange={handleChange} />
-      </label>
-      <button onSubmit={handleSubmit}>submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <div>
+            <p>TÍTULO:</p>
+          </div>
+          <input name="title" type="text" onChange={handleChange} />
+        </label>
+        <label>
+          <div>
+            <p>DESCRIÇÃO:</p>
+          </div>
+          <input name="description" type="text" onChange={handleChange} />
+        </label>
+        <button onSubmit={handleSubmit}>submit</button>
+      </form>
+      {onSubmit ? <Navigate to="/events" /> : ""}
+    </div>
   );
 };
 
--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 import "../styles/videos.scss";
 import Card from "./Card";
 import { DataContext } from "../providers/data";
+import db from "../firebase";
+import { collection, addDoc, onSnapshot } from "firebase/firestore";
 
 function EventList(props) {
   const { data } = React.useContext(DataContext);
-
-  console.log(data);
 
   const arr = data.events.map((doc) => {
     return <Card title={doc.title} description={doc.description}></Card>;
