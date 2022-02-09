76f4bb5649714e150e204954ec41b1782fbf5035
pdrsvr <pedro.hbessa@live.com>
2022-02-09 18:39:16 -0300

    some styles

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -2,12 +2,23 @@ import "../styles/event-screen.scss";
 import { useParams } from "react-router-dom";
 import React from "react";
 import { DataContext } from "../providers/data";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
+import "../styles/panel.scss";
 
 function Panel(props) {
+  const auth = getAuth();
   let params = useParams();
-  console.log(props);
+  console.log(auth.currentUser);
 
-  return <div>PANEL</div>;
+  return (
+    <div className="panel">
+      <h1>BEM VINDO {auth.currentUser.email}</h1>
+    </div>
+  );
 }
 
 export default Panel;
